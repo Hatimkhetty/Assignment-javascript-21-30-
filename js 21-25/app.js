00fed4229a1bf14a5630807804c97d325e76049b
hatimkhetty <hatimfkhetty53@gmail.com>
2020-06-22 22:48:57 +0500

    21-30 done

---
+++ js 21-25/app.js
@@ -0,0 +1,15 @@
+//.............assignment 21-25..........//
+
+// Q1 //
+var firstname = prompt("Enter your first name");
+var lastname = prompt("Enter your first last name");
+var fullname = firstname + "" + lastname;
+alert("Greet:  " + fullname);
+document.write("</br>");
+
+// Q2 //
+var mobileModel = prompt("Enter your favorite Mobile Phone Model");
+var lenght = mobileModel.length;
+document.write(" my favorite mobile phone model is : " + mobileModel);
+document.write("</br>");
+document.write("Lenght of string :" + lenght);
