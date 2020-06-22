23af2f142396c6d978de20336aa21c2df53ce0e1
hatimkhetty <hatimfkhetty53@gmail.com>
2020-06-22 23:01:30 +0500

    done

--- js 21-25/app.js
+++ js 21-25/app.js
@@ -1,15 +1,23 @@
-//.............assignment 21-25..........//
+//.............assignment 21-25..........
 
-// Q1 //
+// Q1 
 var firstname = prompt("Enter your first name");
 var lastname = prompt("Enter your first last name");
 var fullname = firstname + "" + lastname;
 alert("Greet:  " + fullname);
 document.write("</br>");
 
-// Q2 //
+// Q2  
 var mobileModel = prompt("Enter your favorite Mobile Phone Model");
 var lenght = mobileModel.length;
 document.write(" my favorite mobile phone model is : " + mobileModel);
 document.write("</br>");
 document.write("Lenght of string :" + lenght);
+
+// Q3 
+
+document.write("<br/><br/>");
+var str = "Pakistan";
+var index = str.indexOf("n");
+document.write("String " + str + "<br/>");
+document.write("Index of 'n' is: " + index);
